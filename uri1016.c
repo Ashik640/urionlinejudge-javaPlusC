b3ffbe7aef1649ed7e3b8ed16242959542d858f7
Akhtaruzzaman Ashik <36150077+Ashik640@users.noreply.github.com>
2018-03-24 20:23:06 +0600

    Add files via upload

---
+++ uri1016.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int d, m;
+    scanf("%d", &d);
+    m = d * 2;
+    printf("%d minutos\n", m);
+
+    return 0;
+}
---
+++ uri1017.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int t, s;
+    float f;
+    scanf("%d %d", &t, &s);
+    f = t * s / 12.0;
+    printf("%.3f\n", f);
+
+    return 0;
+}
